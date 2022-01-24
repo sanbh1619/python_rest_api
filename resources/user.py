af1c80f86db99ac5d42cec153e6e896a59cedb1e
Sanjay Bhosale <sanjay.bhosale@druva.com>
2022-01-24 17:11:45 +0530

    first commit

---
+++ resources/user.py
@@ -0,0 +1,19 @@
+from flask_restful import Resource, reqparse
+
+from models.user import UserModel
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help='name of user required for login')
+    parser.add_argument('password', type=str, required=True, help='password of user required for login')
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {'message': "User with name {} already exists".format(data['username'])}, 400
+
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return {"message": "User created successfully"}, 201
